aff56bb693c20b66537b4ab7389ceb7f1bdf3416
Sudhanva Bharadwaj BM <bmsv245@gmail.com>
2025-06-13 13:37:39 +0530

    small changes

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
-from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(title='Life')
