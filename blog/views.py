dfc070abc479316a037c4f0f478f3076086d83c4
Sudhanva Bharadwaj BM <bmsv245@gmail.com>
2025-06-13 10:25:21 +0530

    Third commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 # Create your views here.
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(title='Life')
+    return render(request, 'blog/post_list.html', {'posts': posts})
