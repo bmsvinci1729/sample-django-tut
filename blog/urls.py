1ce32cdeb9347d4e596d447bc8780071986e4478
Sudhanva Bharadwaj BM <bmsv245@gmail.com>
2025-06-13 11:37:45 +0530

    panchvi commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views # includeing all      our views here
 
 urlpatterns = [
     path('', views.post_list, name = 'post_list'), # 
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 ] 
